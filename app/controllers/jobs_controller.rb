1a93499be2df27f799ae67e6685dc45ebae637ba
xurongrong <xurongrong0821@gmail.com>
2017-03-31 10:32:32 +0800

    order by wage

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,7 +1,15 @@
 class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+
   def index
-    @jobs = Job.where(:is_hidden => false).order("created_at DESC")
+    @jobs = case params[:order]
+     when 'by_lower_bound'
+       Job.published.order('wage_lower_bound DESC')
+     when 'by_upper_bound'
+       Job.published.order('wage_upper_bound DESC')
+     else
+       Job.published.recent
+     end
   end
 
   def show
@@ -10,6 +18,8 @@ class JobsController < ApplicationController
     if @job.is_hidden
       flash[:warning] = "This Job already archieved"
       redirect_to root_path
+    end
+
   end
 
   def new
@@ -46,6 +56,7 @@ class JobsController < ApplicationController
 
 
   private
+
     def job_params
       params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email, :is_hidden)
     end
